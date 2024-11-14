c35db4e6b237222cab1510bfee068b85e383d742
Starlight757 <132366132+Starlight757@users.noreply.github.com>
2024-11-14 23:21:38 +0300

    v3

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require ('dotenv').config();
 const { Bot } = require('grammy');
-
+console.log("123");
 const bot = new Bot(process.env.BOT_API_KEY);
 
 bot.command('start', async (ctx) => {
   //  await ctx.reply('Шалом!')
   await ctx.reply('Privet'+ctx.chatId, ctx.chatId);
 })
-
+//123/
 bot.start();
